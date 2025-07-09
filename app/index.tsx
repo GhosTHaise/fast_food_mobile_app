368cc1fc3d19ca7e74509826e0087fd771acb449
GhosTHaise <ghostrex2@gmail.com>
2025-07-09 21:39:52 +0300

    feat : test flastlist

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,21 @@
-import { FlatList, Text } from "react-native";
+import { offers } from "@/constants";
+import { FlatList, Pressable, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
    <SafeAreaView>
       <FlatList
-      data={["a", "b", "c", "d"]}
-      renderItem={({ item }) => <Text>{item}</Text>}
+      data={offers}
+      renderItem={({ item , index }) => {
+         return (
+          <View>
+            <Pressable className="bg-amber-600">
+                <Text>{item.title}</Text>
+            </Pressable>
+          </View>
+         )
+      }}
       />
    </SafeAreaView>
   );
