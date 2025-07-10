44999ca2abc798e73c174418022281251fb760da
GhosTHaise <ghostrex2@gmail.com name = GhosTHaise>
2025-07-10 21:27:37 +0300

    feat : hide header

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,9 @@
-import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
-   <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-blue-500">
-        Welcome to Nativewind!
-      </Text>
-    </View>
+   <SafeAreaView>
+      
+   </SafeAreaView>
   );
 }
