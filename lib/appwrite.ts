617e1336d118afd90b00423bbcbedc54135f3f24
GhosTHaise <ghostrex2@gmail.com>
2025-07-22 21:01:02 +0300

    feat : start to build auth layout

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -2,5 +2,7 @@
 export const appwriteConfig = {
     endpoint : process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
     platform : "mg.ghost.foodordering",
-    projectId : process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID
+    projectId : process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+    databaseId : process.env.DATABASE_ID,
+    userCollectionId : "687fd003002b7431d68c"
 }
