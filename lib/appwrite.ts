cc8cbd3dfd3fb548dbe2636baf974c70efee6b7e
GhosTHaise <ghostrex2@gmail.com>
2025-07-28 22:45:15 +0300

    feat : signIn

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -5,7 +5,7 @@ export const appwriteConfig = {
     endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT!,
     platform: "mg.ghost.foodordering",
     projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID!,
-    databaseId: process.env.EXPO_PUBLIC_DATABASE_ID!,
+     databaseId: process.env.EXPO_PUBLIC_DATABASE_ID!,
     userCollectionId: "687fd003002b7431d68c"
 }
 
