9399078c3198dddee30a35f517c3094c0543181f
GhosTHaise <ghostrex2@gmail.com>
2025-08-03 20:01:46 +0300

    feat :TabBarIcon

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,6 +1,19 @@
+import { images } from '@/constants';
 import useAuthStore from '@/store/auth.store';
-import { Redirect, Slot } from 'expo-router';
+import { TabBarIconProps } from '@/type';
+import cn from "clsx";
+import { Redirect, Tabs } from 'expo-router';
 import React from 'react';
+import { Image, Text, View } from 'react-native';
+
+const TabBarIcon = ({ focused, icon, title }: TabBarIconProps) => (
+    <View className="tab-icon">
+        <Image source={icon} className="size-7" resizeMode="contain" tintColor={focused ? '#FE8C00' : '#5D5F6D'} />
+        <Text className={cn('text-sm font-bold', focused ? 'text-primary':'text-gray-200')}>
+            {title}
+        </Text>
+    </View>
+)
 
 export default function TabLayout() {
     const {isAuthenticated} = useAuthStore();
@@ -8,6 +21,13 @@ export default function TabLayout() {
     if (!isAuthenticated) return <Redirect href="/sign-in" />;
 
     return (
-        <Slot />
+        <Tabs>
+            <Tabs.Screen 
+            name="index" 
+            options={{
+                title: 'Home',
+                tabBarIcon : ({focused}) => <TabBarIcon focused={focused} icon={images.home} title="Home" />
+            }} />
+        </Tabs>
     )
 }
